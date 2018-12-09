2ff50c41b76d6187487036ed5803f8aac428401c
awgibson <34604592+awgibson@users.noreply.github.com>
2018-12-08 19:58:08 -0600

    fixed firefox issue

--- js/app.js
+++ js/app.js
@@ -10,61 +10,45 @@ window.addEventListener('load', function (event) {
 
     document.querySelector('.search-container').innerHTML = searchHTML;
 
-    for (let i = 0; i < 12; i++) {
-        getRandomUser(i);
-    }
+    getRandomUser();
+
 
 
     function getRandomUser(arrayPosition) {
-        fetch('https://randomuser.me/api/')
+        fetch('https://randomuser.me/api/?results=12')
             .then(response => response.json())
             .catch(error => console.log('There was a problem', error))
-            .then(userInfo => {
-                const allInfo = userInfo.results[0];
-                const user =
-                {
-                    fullName: allInfo.name.first + ' ' + allInfo.name.last,
-                    pictureLarge: allInfo.picture.large,
-                    pictureMedium: allInfo.picture.medium,
-                    email: allInfo.email,
-                    street: allInfo.location.street,
-                    city: allInfo.location.city,
-                    state: allInfo.location.state,
-                    zip: allInfo.location.postcode,
-                    cell: allInfo.cell,
-                    birthday: allInfo.dob.date,
-                    galleryNumber: arrayPosition
-                }
-                users.push(user);
-                buildGalleryCard(user);
-            });
+            .then(users => buildGalleryCard(users))
     };
 
-    function buildGalleryCard(user) {
-        const galleryHTML = `
-    <div id="${user.galleryNumber}" class="card">
+    function buildGalleryCard(users) {
+        console.log(users);
+        for (let i = 0; i < users.results.length; i++) {
+            let user = users.results[i];
+            const galleryHTML = `
+    <div id="${i}" class="card">
         <div class="card-img-container">
-            <img class="card-img" src="${user.pictureMedium}" alt="profile picture">
+            <img class="card-img" src="${user.picture.medium}" alt="profile picture">
         </div>
         <div class="card-info-container">
-            <h3 id="name" class="card-name cap">${user.fullName}</h3>
+            <h3 id="name" class="card-name cap">${user.name.first} ${user.name.last}</h3>
             <p id="email" class="card-text">${user.email}</p>
-            <p class="card-text cap">${user.city}, ${user.state}</p>
+            <p class="card-text cap">${user.location.city}, ${user.location.state}</p>
         </div>
     </div>`;
 
-        let modalHTML = `<div id="${user.galleryNumber}" class="modal-container hide">
+            let modalHTML = `<div id="${i}" class="modal-container hide">
     <div class="modal">
         <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
         <div class="modal-info-container">
-            <img class="modal-img" src="${user.pictureLarge}" alt="profile picture">
-            <h3 id="name" class="modal-name cap">${user.fullName}</h3>
+            <img class="modal-img" src="${user.picture.large}" alt="profile picture">
+            <h3 id="name" class="modal-name cap">${user.name.first} ${user.name.last}</h3>
             <p class="modal-text">${user.email}</p>
-            <p class="modal-text cap">${user.city}</p>
+            <p class="modal-text cap">${user.location.city}</p>
             <hr>
             <p class="modal-text">${user.cell}</p>
-            <p class="modal-text">${user.street}, ${user.city}, ${user.state} ${user.zip}</p>
-            <p class="modal-text">Birthday: ${user.birthday}</p>
+            <p class="modal-text">${user.location.street}, ${user.location.city} ${user.location.state} ${user.location.postcode}</p>
+            <p class="modal-text">Birthday: ${user.dob.date}</p>
         </div>
     </div>
 
@@ -75,8 +59,9 @@ window.addEventListener('load', function (event) {
     </div>
 </div>`;
 
-        document.querySelector('#gallery').insertAdjacentHTML('beforeend', galleryHTML);
-        document.querySelector('#gallery').insertAdjacentHTML('beforebegin', modalHTML);
+            document.querySelector('#gallery').insertAdjacentHTML('beforeend', galleryHTML);
+            document.querySelector('#gallery').insertAdjacentHTML('beforebegin', modalHTML);
+        }
 
     };
 
